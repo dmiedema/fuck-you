9314c832252453a2ceab1c694e1a5f9bb340a178
dmiedema <daniel@househappy.org>
2014-02-28 23:17:26 -0800

    Update for use with antigen

--- fuck.plugin.zsh
+++ fuck.plugin.zsh
@@ -2,6 +2,12 @@ function fuck() {
   [[ -f $ZSH_CUSTOM/plugins/fuck/fuck.plugin.zsh ]] && DIR=$ZSH_CUSTOM/plugins/fuck
   [[ -f $ZSH/plugins/fuck/fuck.plugin.zsh ]] && DIR=$ZSH/plugins/fuck
 
+  if type antigen > /dev/null; then
+    DIR=$_ANTIGEN_INSTALL_DIR/repos/https-COLON--SLASH--SLASH-github.com-SLASH-dmiedema-SLASH-fuck-you.git
+  fi
+
+
+
   if killall -9 "$2"; then
     echo ; echo " (╯°□°）╯︵$(echo "$2"|${DIR}/flip)"; echo
   fi
