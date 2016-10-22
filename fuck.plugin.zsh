b8da6428486d0481a379a1f7fa824c3d881dcf81
dmiedema <danielmiedema+github@me.com>
2016-10-21 17:44:29 -0700

    Add support for zgen
    I've been using zgen for a while now, its probably about time i support
    it properly

--- fuck.plugin.zsh
+++ fuck.plugin.zsh
@@ -4,9 +4,9 @@ function fuck() {
 
   if type antigen > /dev/null; then
     DIR=$_ANTIGEN_INSTALL_DIR/repos/https-COLON--SLASH--SLASH-github.com-SLASH-dmiedema-SLASH-fuck-you.git
+  elif type zgen > /dev/null; then
+    DIR=$ZGEN_DIR/dmiedema/fuck-you-master
   fi
-
-
 
   if killall -9 "$2"; then
     echo ; echo " (╯°□°）╯︵$(echo "$2"|${DIR}/flip)"; echo
